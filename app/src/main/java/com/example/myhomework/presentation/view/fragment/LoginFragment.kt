c17dcf9e33f672e3970a0cb82c94e984de23ea7d
Kovel Uladzimir <lineage200909@mail.ru>
2024-12-18 13:41:45 +0300

    lesson 24 сохраняет ли sharPref

--- app/src/main/java/com/example/myhomework/presentation/view/fragment/LoginFragment.kt
+++ app/src/main/java/com/example/myhomework/presentation/view/fragment/LoginFragment.kt
@@ -10,13 +10,16 @@ import android.widget.TextView
 import android.widget.Toast
 import androidx.fragment.app.Fragment
 import com.example.myhomework.R
+import com.example.myhomework.data.repository.UserSharedPref
 import com.example.myhomework.databinding.FragmentLoginBinding
 import com.example.myhomework.domain.use_case.isEmailValid
+import org.koin.android.ext.android.inject
 
 class LoginFragment : Fragment() {
 
     private var _binding: FragmentLoginBinding? = null
     private val binding get() = _binding!!
+    private val sharedPref : UserSharedPref by inject()
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -68,8 +71,24 @@ class LoginFragment : Fragment() {
                 ).show()
                 false
             }
+
+            !isEmailExist(emailString, validationResPasswordSignup) -> {
+                Toast.makeText(context, "Incorrect Email", Toast.LENGTH_LONG).show()
+                false
+            }
             else -> true
         }
+    }
+
+    private fun isEmailExist(email: String, password : String): Boolean {
+        val userData = sharedPref.getUser()
+        val savedEmail = userData["email"]
+        val savedPassword = userData["password"]
+        return if (savedEmail == email) {
+            true
+        } else if(savedPassword == password){
+            true
+        } else false
     }
 
     private fun setupListener(){
@@ -90,14 +109,14 @@ class LoginFragment : Fragment() {
 
         loginButtonMain?.setOnClickListener {
             parentFragmentManager.beginTransaction()
-                .replace(R.id.newFragmentView, MainFragment(), "Login")
+                .replace(R.id.newFragmentView, MainFragment(), "Main")
                 .addToBackStack(null)
                 .commit()
         }
 
         accTextviewLogin?.setOnClickListener {
             parentFragmentManager.beginTransaction()
-                .replace(R.id.newFragmentView, SignUpFragment(), "Login")
+                .replace(R.id.newFragmentView, SignUpFragment(), "SignUp")
                 .addToBackStack(null)
                 .commit()
         }
